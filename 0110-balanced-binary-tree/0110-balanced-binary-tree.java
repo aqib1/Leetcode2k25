4f568195859569e1f54c275481225cec20d6428c
Aqib Javed <aqibbutt3078@gmail.com>
2025-01-12 20:18:23 +0000

    Time: 0 ms (100%), Space: 44.6 MB (30.23%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -19,17 +19,17 @@ class Solution {
     }
 
     public int height(TreeNode root) {
- if(root == null)
+        if (root == null)
             return 0;
 
         int left = height(root.left);
         int right = height(root.right);
 
-        if(left == -1 || right == -1) {
+        if (left == -1 || right == -1) {
             return -1;
         }
 
-        if(Math.abs(left - right) > 1) {
+        if (Math.abs(left - right) > 1) {
             return -1;
         }
 
