8520ea276c582340fbbb9a00c88e287d11ebb1e1
Aqib Javed <aqibbutt3078@gmail.com>
2025-01-12 19:46:41 +0000

    Time: 23 ms (8.49%), Space: 44.7 MB (98.66%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+
 class Solution {
     public int singleNumber(int[] nums) {
+        var map = Arrays.stream(nums)
+        .boxed()
+        .collect(
+            Collectors.groupingBy(
+                    s -> s,
+                    LinkedHashMap::new,
+                    Collectors.counting()
+                    )
+                );
         
+        return Math.toIntExact(map.entrySet().stream()
+                .filter(entry -> entry.getValue() == 1L)
+                .map(Map.Entry::getKey)
+                .findFirst().orElse(1));
     }
 }
