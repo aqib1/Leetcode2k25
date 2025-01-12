19127d430f67c03b94dc3a8b722d45e9fe25dd47
Aqib Javed <aqibbutt3078@gmail.com>
2025-01-12 21:52:27 +0000

    Time: 0 ms (100%), Space: 4.1 MB (26.67%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+    data := make([]int, 0, 10)
+	inorder(root, &data)
+	return data
+}
+
+func inorder(root *TreeNode, data *[]int) {
+	if root == nil {
+		return
+	}
+
+	inorder(root.Left, data)
+	*data = append(*data, root.Val)
+	inorder(root.Right, data)
+}
