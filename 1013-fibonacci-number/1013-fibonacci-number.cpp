ae0f52ee8c56aa42b25da4ed2622c5d3137865c6
Aqib Javed <aqibbutt3078@gmail.com>
2025-02-14 22:07:32 +0000

    Time: 0 ms (100%), Space: 8.3 MB (9.78%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,12 +1,13 @@
 #include "iostream"
 using namespace std;
 
+// Time complexity O(n) and space O(n)
 class Solution {
 public:
     int fib(int n) {
         if(n == 0)
             return 0;
-            
+
         vector<int> memoization(n+1);
         memoization[0] = 0;
         memoization[1] = 1;
