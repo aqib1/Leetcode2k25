64197586bff7eeacf632b5178c766fc5f317394d
Aqib Javed <aqibbutt3078@gmail.com>
2025-02-14 21:54:12 +0000

    Time: 0 ms (100%), Space: 40.6 MB (37.25%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        return fib(n, 0, 1);
+    }
+
+    public int fib(int n, int a, int b) {
+        if(n == 0)
+            return a;
+        if(n == 1)
+            return b;
+
+        return fib(n - 1, b, a+b);        
+    }
+}
