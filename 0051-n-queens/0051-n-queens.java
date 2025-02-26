4d4d1e3f11cd497091584361d34256ec56e6cf4e
Aqib Javed <aqibbutt3078@gmail.com>
2025-02-26 18:00:07 +0000

    Time: 2 ms (88.57%), Space: 44.9 MB (55.87%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,92 +1,64 @@
 class Solution {
-    // Time complexity O(N!) and Space complexity O(n2)
-    private final List<List<String>> response = new ArrayList<>();
-    private char[][] board;
+    private final List<List<String>> result = new ArrayList<>();
     private int numberOfQueens;
-
+    private char[][] board;
     public List<List<String>> solveNQueens(int n) {
         this.numberOfQueens = n;
         initBoard();
-        setQueens(0, 0, this.numberOfQueens);
-        return response;
+        setQueens(0);
+        return result;
     }
 
-        private void setQueens(int row, int col, int n) {
-        if(col == this.numberOfQueens) {
-            col = 0;
-            row++;
-        }
-
-        if(n == 0) {
-            response.add(createBoard(board));
+    private void setQueens(int row) {
+        if(row == numberOfQueens) {
+            prepareBoard();
             return;
         }
 
-        if(row == this.numberOfQueens) {
-            return;
+        for(int column = 0; column < numberOfQueens; column++) {
+            if(isSafe(row, column)) {
+                this.board[row][column] = 'Q';
+                setQueens(row + 1);
+                this.board[row][column] = '.';
+            }
         }
-
-        if(isValidMove(row, col)) {
-            board[row][col] = 'Q';
-            n--;
-            setQueens(row, col + 1, n);
-            n++;
-            board[row][col] = '.';
-        }
-
-        setQueens(row, col + 1, n);
     }
 
-    private boolean isValidMove(int row, int col) {
-        // top-down and left-right check
-        for(int i = 0; i < numberOfQueens; i++) {
-            if(board[i][col] == 'Q') {
-                return false;
-            }
-
-            if(board[row][i] == 'Q') {
-                return false;
-            }
-        }
-
-        // check diagonal from top left to current and current to bottom right
-        for(int r=row, c=col; c>=0 && r>=0; c--, r--) {
-            if(board[r][c] == 'Q')
-                return false;
-        }
-        for(int r=row, c=col; c<numberOfQueens && r<numberOfQueens; c++, r++) {
-            if(board[r][c] == 'Q')
+    private boolean isSafe(int row, int column) {
+        // check top
+        for(int r = row; r >= 0; r--) {
+            if(board[r][column] == 'Q')
                 return false;
         }
 
-        // check diagonal from current to bottom left and current to top right
-        for(int r=row, c=col; r<numberOfQueens && c>=0; r++, c--) {
-            if(board[r][c] == 'Q')
+        // check top left
+        for(int r = row, col = column; r >= 0 && col >= 0; r--, col--) {
+            if(board[r][col] == 'Q')
                 return false;
         }
 
-        for(int r=row, c=col; r>=0 && c<numberOfQueens; r--, c++) {
-            if(board[r][c] == 'Q')
+        // check top right
+        for(int r = row, col = column; r >= 0 && col < numberOfQueens; r--, col++) {
+            if(board[r][col] == 'Q')
                 return false;
         }
 
         return true;
     }
 
-    private List<String> createBoard(char[][] board) {
-        var res = new ArrayList<String>();
-        for(var data : board) {
-            res.add(new String(data));
+    private void prepareBoard() {
+        var prepare = new ArrayList<String>();
+        for(var b: board) {
+            prepare.add(new String(b));
         }
-        return res;
+
+        result.add(prepare);
     }
 
     private void initBoard() {
         this.board = new char[numberOfQueens][numberOfQueens];
         for(int i = 0; i < numberOfQueens; i++) {
-            for(int j = 0; j < numberOfQueens; j++) {
-                this.board[i][j] = '.';
-            }
+            Arrays.fill(this.board[i], '.');
         }
     }
 }
