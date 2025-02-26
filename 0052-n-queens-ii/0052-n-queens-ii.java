43eb5981f6bfd77b6b7bf13bcc1fe5e38246bbcf
Aqib Javed <aqibbutt3078@gmail.com>
2025-02-26 18:12:55 +0000

    Time: 2 ms (57.63%), Space: 40.5 MB (54.24%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    private int count;
+    private char[][] board;
+    private int numberOfQueens;
+    
+    public int totalNQueens(int n) {
+        this.numberOfQueens = n;
+        initBoard();
+        setQueens(0);
+        return count;
+    }
+
+        private void initBoard() {
+        this.board = new char[numberOfQueens][numberOfQueens];
+        for(int i = 0; i < numberOfQueens; i++) {
+            Arrays.fill(board[i], '.');
+        }
+    }
+
+    public void setQueens(int row) {
+        if(row == numberOfQueens) {
+            count++;
+            return;
+        }
+
+        for(int column = 0; column < numberOfQueens; column++) {
+            if(isSafe(row, column)) {
+                board[row][column] = 'Q';
+                setQueens(row + 1);
+                board[row][column] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(int row, int column) {
+        for(int r = row; r >= 0; r--)
+            if(board[r][column] == 'Q')
+                return false;
+
+        for(int r = row, col = column; r >= 0 && col >= 0; r--, col--) {
+            if(board[r][col] == 'Q')
+                return false;
+        }
+
+        for(int r = row, col = column; r >= 0 && col < numberOfQueens; r--, col++) {
+            if(board[r][col] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+}
