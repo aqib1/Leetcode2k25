16797f2997fb6933bca86a3d35d278c79cd72a9d
Aqib Javed <aqibbutt3078@gmail.com>
2025-02-26 22:11:34 +0000

    Time: 27 ms (14.72%), Space: 45.8 MB (6.65%) - LeetHub

---
+++ 0965-unique-email-addresses/0965-unique-email-addresses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+          var set = new HashSet<String>();
+        for(var email: emails) {
+            var emailSplit = email.split("@");
+            var localName = emailSplit[0];
+            var domainName = emailSplit[1];
+            localName = applyRules(localName);
+            set.add(localName + "@" + domainName);
+        }
+        return set.size();
+    }
+
+    private String applyRules(String localName) {
+        return localName
+                .replaceAll("\\.", "")
+                .split("\\+")[0];
+    }
+}
