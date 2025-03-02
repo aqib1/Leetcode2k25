674862b94883a04fce1240b9af3934d1bdad3f7f
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-02 19:01:36 +0000

    Time: 5 ms (72.85%), Space: 44 MB (75.3%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -5,11 +5,9 @@ class Solution {
 
     private int reverse(int x) {
         int reverse = 0;
-        int mul = 1;
         while(x != 0) {
-            reverse = reverse * mul + (x % 10);
+            reverse = reverse * 10 + (x % 10);
             x /= 10;
-            mul = 10;
         }
 
         return reverse;
