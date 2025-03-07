f5cd279624cd6b168a9746e63637ccebfe5860c7
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-07 23:18:50 +0000

    Time: 47 ms (18.06%), Space: 50.8 MB (80.12%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,20 +1,24 @@
 import java.util.AbstractMap;
+
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        var minHeap = new PriorityQueue<Map.Entry<int[], Double>>(
-                Map.Entry.comparingByValue()
-        );
+        var maxHeap = new PriorityQueue<Map.Entry<int[], Double>>(
+                (e1, e2) -> Double.compare(e2.getValue(), e1.getValue()));
         var result = new int[k][2];
-        for(int [] point : points) {
+        for (int[] point : points) {
             // we need to find distance of each point from origin (0,0)
             var distance = Math.sqrt(
-                    Math.pow(point[0], 2) + Math.pow(point[1], 2)
-            );
-            minHeap.offer(new AbstractMap.SimpleEntry<>(point, distance));
+                    Math.pow(point[0], 2) + Math.pow(point[1], 2));
+            if (maxHeap.size() < k)
+                maxHeap.offer(new AbstractMap.SimpleEntry<>(point, distance));
+            else if (maxHeap.peek().getValue() > distance) {
+                maxHeap.poll();
+                maxHeap.offer(new AbstractMap.SimpleEntry<>(point, distance));
+            }
         }
 
-        for(int i = 0; i < k && !minHeap.isEmpty(); i++) {
-            result[i] = minHeap.poll().getKey();
+        for (int i = 0; i < k && !maxHeap.isEmpty(); i++) {
+            result[i] = maxHeap.poll().getKey();
         }
 
         return result;
