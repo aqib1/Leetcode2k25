9e6a5661d16a8796ad1d74b0ef2cd13e0dae3aef
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-14 09:54:08 +0000

    Time: 0 ms (100%), Space: 40.5 MB (48.27%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,17 @@
 class Solution {
     public int fib(int n) {
         if(n == 0)
-            return 0;
-            
-        int[] memoization = new int[n + 1];
-        memoization[0] = 0;
-        memoization[1] = 1;
-        return fib(n, memoization);
+            return n;
+        
+        var tabulation = new int[n + 1];
+        tabulation[0] = 0;
+        tabulation[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            tabulation[i] = tabulation[i - 1] + tabulation[i - 2];
+        }
+
+        return tabulation[n];
     }
 
     public int fib(int n, int[] memoization) {
