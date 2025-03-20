257ad66eb3e52a8b5d02fb4686f26e734efafbe9
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-20 05:12:24 +0000

    Time: 7 ms (66.34%), Space: 47.5 MB (97.71%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    // time complexity ON(logN) and space O(N)
+    public List<List<String>> groupAnagrams(String[] strs) {
+        var groupAnagrams = new HashMap<String, List<String>>();
+        for(var str: strs) {
+            var key = getSortedKey(str);
+            if(groupAnagrams.containsKey(key)) {
+                groupAnagrams.get(key).add(str);
+            } else {
+                groupAnagrams.put(key, new ArrayList<>(List.of(str)));
+            }
+        }
+
+        return new ArrayList<>(groupAnagrams.values());
+    }
+
+    private String getSortedKey(String str) {
+        var data = str.toCharArray();
+        Arrays.sort(data);
+        return new String(data);
+    }
+}
