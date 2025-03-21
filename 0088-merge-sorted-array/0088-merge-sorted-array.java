8ee5a6a26d3fa0403cd47e09d49d520b8a23b245
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-21 21:17:39 +0000

    Time: 0 ms (100%), Space: 42.4 MB (42.18%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,23 @@
 class Solution {
     // Time complexity O(n + m) and space O(n + m)
     public void merge(int[] num1, int m, int[] num2, int n) {
-        int p = m + n - 1;
-        int p1 = m - 1;
-        int p2 = n - 1;
-
-        while(p >= 0) {
+       int p = m + n - 1;
+       int p1 = m - 1;
+       int p2 = n - 1;
+       
+       while(p >= 0) {
+            // in case second array is empty we do not need to proceed
             if(p2 < 0)
                 break;
-            if(p1 >=0 && num1[p1] > num2[p2]) {
+
+            // if num1 is not empty and num1 is greater than num2
+            if(p1 >= 0 && num1[p1] > num2[p2]) {
                 num1[p] = num1[p1--];
             } else {
                 num1[p] = num2[p2--];
-            }
+            }    
+
             p--;
-        }
+       }
     }
 }
