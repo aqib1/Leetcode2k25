cce3ce7f8f6d50a895806519e5ef6911fd3f14b8
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-22 16:46:57 +0000

    Time: 0 ms (100%), Space: 57.1 MB (85.13%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,19 +1,15 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-if(k < 0)
+        if(k < 0)
             throw new IllegalArgumentException();
 
-        int rotateLimit = k < nums.length ?
-                k : k % nums.length;
-
-        if(rotateLimit == 0)
-            return;
+        k = k % nums.length;
 
         int []rotate = new int[nums.length * 2];
         System.arraycopy(nums, 0, rotate, 0, nums.length);
         System.arraycopy(nums, 0, rotate, nums.length, nums.length);
 
-        System.arraycopy(rotate, nums.length - rotateLimit, nums, 0, nums.length);
+        System.arraycopy(rotate, nums.length - k, nums, 0, nums.length);
 
     }
 }
