a48e0b16670833570558f17e20b22debda352928
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-25 23:38:05 +0000

    Time: 2 ms (95.33%), Space: 44.6 MB (63.05%) - LeetHub

--- 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] sortEvenOdd(int[] nums) {
-int evenLen = nums.length % 2 == 0 ?
+        int evenLen = nums.length % 2 == 0 ?
                 nums.length / 2 : (nums.length / 2) + 1;
         int oddLen = nums.length / 2;
 
@@ -16,24 +16,16 @@ int evenLen = nums.length % 2 == 0 ?
             }
         }
 
-        odd = Arrays.stream(odd)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .mapToInt(Integer::intValue)
-                .toArray();
-        even = Arrays.stream(even)
-                .boxed()
-                .sorted()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        Arrays.sort(even);
+        Arrays.sort(odd);
 
 
-        j = 0; k = 0;
+        j = 0; k = odd.length - 1;
         for(int i = 0; i < nums.length; i++) {
             if(i % 2 == 0) {
                 nums[i] = even[j++];
             } else {
-                 nums[i] = odd[k++];
+                 nums[i] = odd[k--];
             }
         }
 
