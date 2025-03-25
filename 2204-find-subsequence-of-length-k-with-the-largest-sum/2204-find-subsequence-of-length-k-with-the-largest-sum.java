536053d630617143af178b076b18fac5c57d3260
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-25 23:08:03 +0000

    Time: 8 ms (61.22%), Space: 44.6 MB (71.95%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -2,26 +2,24 @@ class Solution {
     // ON(LogK) and space O(K)
     public int[] maxSubsequence(int[] nums, int k) {
         var result = new int[k];
-        var minHeap = new PriorityQueue<Integer>(k);
-
-        for(int num: nums) {
-            minHeap.offer(num);
+        var minHeap = new PriorityQueue<int[]>(k, Comparator.comparingInt(a -> a[0]));
+        for(int i = 0; i < nums.length; i++) {
+            minHeap.offer(new int[] {nums[i], i});
             if(minHeap.size() > k) {
-                minHeap.poll();
+                minHeap.poll(); // keep memory limited to k
             }
+        }
+        var indexes = new HashSet<Integer>();
+        while(!minHeap.isEmpty()) {
+            indexes.add(minHeap.poll()[1]);
         }
 
         int index = 0;
-        for(int num: nums) {
-            if(k == 0)
-                break;
-            if(minHeap.contains(num)) {
-                result[index++] = num;
-                minHeap.remove(num);
-                k--;
+        for(int i = 0; i < nums.length; i++) {
+            if(indexes.contains(i)) {
+                result[index++] = nums[i];
             }
         }
-
         return result;
     }
 }
