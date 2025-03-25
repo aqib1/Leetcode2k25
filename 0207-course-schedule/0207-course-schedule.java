ec7cbb52ae0649b947fced6f058eecaff4a136c2
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-25 19:53:23 +0000

    Time: 4 ms (91.37%), Space: 45 MB (83.35%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        var graph = constructGraph(numCourses, prerequisites);
+        var visited = new boolean[numCourses];
+        var inStack = new boolean[numCourses];
+
+        for (int v = 0; v < graph.size(); v++) {
+            if (!visited[v] && hasCycle(v, graph, visited, inStack)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(
+            int v,
+            ArrayList<ArrayList<Integer>> graph,
+            boolean[] visited,
+            boolean[] inStack) {
+        if (inStack[v])
+            return true;
+
+        visited[v] = true;
+        inStack[v] = true;
+
+        for (int edge : graph.get(v)) {
+            if (!visited[edge]
+                    && hasCycle(edge, graph, visited, inStack)) {
+                return true;
+            } else if (inStack[edge]) {
+                return true;
+            }
+        }
+
+        // backtrack
+        inStack[v] = false;
+        return false;
+    }
+
+    public ArrayList<ArrayList<Integer>> constructGraph(int numCourses, int[][] prerequisites) {
+        var graph = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] p : prerequisites) {
+            graph.get(p[0]).add(p[1]);
+        }
+
+        return graph;
+    }
+}
