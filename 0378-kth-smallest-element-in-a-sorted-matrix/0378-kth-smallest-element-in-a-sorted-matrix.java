c8712d36ec2727bd7ca04cb4d0685be0e8342ae9
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-26 03:05:54 +0000

    Time: 0 ms (100%), Space: 50 MB (28.16%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,11 +1,15 @@
 class Solution {
+    // We will use range based binary search
+    // Time complexity O(NLog(max - min)) and space O(1)
     public int kthSmallest(int[][] matrix, int k) {
-        int low = matrix[0][0];
-        int high = matrix[matrix.length - 1][matrix.length - 1];
-        while (low < high) {
+        int low = matrix[0][0]; // smallest number
+        int high = matrix[matrix.length - 1][matrix.length - 1]; // largest number
+
+        // let's use all number in between (max - min)
+        while(low < high) {
             int mid = low + (high - low) / 2;
-            int countLessOrEqual = countLessOrEqual(matrix, mid);
-            if(countLessOrEqual < k) {
+            int countLessThanOrEqual = countLessThanOrEqual(mid, matrix);
+            if(countLessThanOrEqual < k) {
                 low = mid + 1;
             } else {
                 high = mid;
@@ -15,10 +19,10 @@ class Solution {
         return low;
     }
 
-    public int countLessOrEqual(int[][] matrix, int mid) {
+    private int countLessThanOrEqual(int mid, int[][] matrix) {
+        int count = 0;
         int column = matrix.length - 1;
         int row = 0;
-        int count = 0;
 
         while(column >= 0 && row < matrix.length) {
             if(matrix[row][column] <= mid) {
