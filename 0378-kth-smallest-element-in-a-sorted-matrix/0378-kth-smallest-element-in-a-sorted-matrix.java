ac8bec1dad469267468ac8b894627903692ee65d
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-26 02:53:30 +0000

    Time: 0 ms (100%), Space: 50.3 MB (8.64%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,16 +1,34 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        var maxHeap = new PriorityQueue<Integer>(k, Comparator.reverseOrder());
-
-        for(int []m : matrix) {
-            for(int i: m) {
-                maxHeap.offer(i);
-                if(maxHeap.size() > k) {
-                    maxHeap.poll();
-                }
+        int low = matrix[0][0];
+        int high = matrix[matrix.length - 1][matrix.length - 1];
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int countLessOrEqual = countLessOrEqual(matrix, mid);
+            if(countLessOrEqual < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
 
-        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+        return low;
+    }
+
+    public int countLessOrEqual(int[][] matrix, int mid) {
+        int column = matrix.length - 1;
+        int row = 0;
+        int count = 0;
+
+        while(column >= 0 && row < matrix.length) {
+            if(matrix[row][column] <= mid) {
+                count += (column + 1);
+                row++;
+            } else {
+                column--;
+            }
+        }
+
+        return count;
     }
 }
