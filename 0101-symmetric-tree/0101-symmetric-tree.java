6e8438c39d71ff8c86b39672144454b69d8ad10e
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-28 00:38:09 +0000

    Time: 1 ms (13.79%), Space: 41.8 MB (52.58%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,38 +1,45 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null)
-            return true;
-
-        return isSymmetric(root, root);
-    }
-
-    public boolean isSymmetric(TreeNode left, TreeNode right) {
-        if(left == null && right == null) {
-            return true;
-        }
-
-        if(left == null || right == null) {
+       if (root == null)
             return false;
+        Queue<TreeNode> fifo = new LinkedList<>();
+        fifo.add(root.left);
+        fifo.add(root.right);
+
+        while (!fifo.isEmpty()) {
+            var left = fifo.poll();
+            var right = fifo.poll();
+
+            if (left == null && right == null)
+                continue;
+
+            if(left == null || right == null)
+                return false;
+
+            if (left.val != right.val)
+                return false;
+
+            fifo.add(left.left);
+            fifo.add(right.right);
+            fifo.add(left.right);
+            fifo.add(right.left);
         }
 
-        return left.val == right.val &&
-            isSymmetric(left.left, right.right) &&
-            isSymmetric(left.right, right.left);
+        return true;
     }
-
 }
