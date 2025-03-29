52b96c2c5a3d6416eff6d4958fe3d9870eb8a16f
Aqib Javed <aqibbutt3078@gmail.com>
2025-03-29 23:54:44 +0000

    Time: 2 ms (82.8%), Space: 44.7 MB (86.45%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        var charCount = new HashMap<Character, Integer>();
-        for(var ch: magazine.toCharArray()) {
-            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        var count = new int[26];
+        for(var m: magazine.toCharArray()) {
+            count[m - 'a']++;
         }
-
-        for(var ch: ransomNote.toCharArray()) {
-            if(!charCount.containsKey(ch))
+        for(var r: ransomNote.toCharArray()) {
+            if(--count[r - 'a'] < 0) {
                 return false;
-
-            charCount.put(ch, charCount.get(ch) - 1);
-
-            if(charCount.get(ch) < 0)
-                return false;
+            }
         }
-
         return true;
     }
 }
