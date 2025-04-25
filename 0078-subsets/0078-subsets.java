b701cb62d842b77c1bc641073e80579de5e50794
Aqib Javed <aqibbutt3078@gmail.com>
2025-04-25 22:29:07 +0100

    Time: 1 ms (51.59%), Space: 43.2 MB (10.39%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,19 @@
 class Solution {
     private int limit;
     private final List<List<Integer>> output = new ArrayList<>();
-    
+
     public List<List<Integer>> subsets(int[] nums) {
         this.limit = nums.length;
-        var currentCombo = new ArrayList<Integer>();
-        generateSubsetsBacktrack(0, currentCombo, nums);
+        List<Integer> currentCombo = new ArrayList<>();
+        createSubsetsUsingBacktracking(0, currentCombo, nums);
         return output;
     }
 
-    private void generateSubsetsBacktrack(int current, ArrayList<Integer> currentCombo, int[] nums) {
+    private void createSubsetsUsingBacktracking(int current, List<Integer> currentCombo, int[] data) {
         output.add(new ArrayList<>(currentCombo));
-        for(int i = current; i < limit; i++) {
-            currentCombo.add(nums[i]);
-            generateSubsetsBacktrack(i + 1, currentCombo, nums);
+        for (int i = current; i < limit; i++) {
+            currentCombo.add(data[i]);
+            createSubsetsUsingBacktracking(i + 1, currentCombo, data);
             currentCombo.removeLast();
         }
     }
