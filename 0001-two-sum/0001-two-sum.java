4d153149de8fe66812ea788436dbab9bcd4c4aa6
Aqib Javed <aqibbutt3078@gmail.com>
2025-04-26 08:42:36 +0100

    Time: 2 ms (98.83%), Space: 45.1 MB (30.44%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        var deltaByIndexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int delta = target - nums[i];
+            if (deltaByIndexMap.containsKey(delta)) {
+                return new int[]{i, deltaByIndexMap.get(delta)};
+            } else {
+                deltaByIndexMap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
