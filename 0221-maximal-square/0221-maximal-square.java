bd04df2a3ac96acfccf52869c96340554daef8f3
Aqib Javed <aqibbutt3078@gmail.com>
2025-05-01 13:15:43 +0100

    Time: 9 ms (36.27%), Space: 60.2 MB (72.8%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,15 +1,14 @@
 class Solution {
-    private int row;
-    private int col;
+    private int rows;
+    private int columns;
     private int max;
     private char[][] matrix;
     private Integer[][] memo;
 
     public int maximalSquare(char[][] matrix) {
-        row = matrix.length;
-        col = matrix[0].length;
-        memo = new Integer[row][col];
-        memo = new Integer[row][col];
+        this.rows = matrix.length;
+        this.columns = matrix[0].length;
+        this.memo = new Integer[rows][columns];
         this.matrix = matrix;
 
         helper(0, 0);
@@ -17,24 +16,26 @@ class Solution {
         return max * max;
     }
 
-    private int helper(int row, int col) {
-        if (row >= this.row || col >= this.col)
+    private int helper(int row, int column) {
+        if (row >= this.rows || column >= this.columns)
             return 0;
 
-        if (memo[row][col] != null)
-            return memo[row][col];
+        if (memo[row][column] != null) {
+            return memo[row][column];
+        }
 
-        int right = helper(row, col + 1);
-        int diagonal = helper(row + 1, col + 1);
-        int down = helper(row + 1, col);
-
+        int right = helper(row, column + 1);
+        int diagonal = helper(row + 1, column + 1);
+        int down = helper(row + 1, column);
         int min = Math.min(right, Math.min(diagonal, down));
-        int currentValue = matrix[row][col] == '1' ? 1 : 0;
+
+        int currentValue = matrix[row][column] == '1' ? 1 : 0;
         if (currentValue == 1) {
             currentValue += min;
         }
-        memo[row][col] = currentValue;
+        memo[row][column] = currentValue;
         max = Math.max(max, currentValue);
+
         return currentValue;
     }
 }
