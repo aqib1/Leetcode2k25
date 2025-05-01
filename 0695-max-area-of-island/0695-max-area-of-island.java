fa111e363df3b4ddf81434fb88689a2f3012511f
Aqib Javed <aqibbutt3078@gmail.com>
2025-05-01 12:54:29 +0100

    Time: 2 ms (70.86%), Space: 44.4 MB (81.95%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,40 +1,39 @@
 class Solution {
-    private static final int[] DIRECTION = { 0, -1, 0, 1, 0 };
-    private int row;
-    private int colum;
-    private int currentArea;
+    private static final int[] DIRECTIONS = {-1, 0, 1, 0, -1};
+    private int rows;
+    private int columns;
     private int maxArea;
+    private int currentArea;
     private int[][] grid;
-
- // Time complexity O(n + m) and space O(n + m)
+    
+    // Time complexity O(n + m) and space O(n + m)
     public int maxAreaOfIsland(int[][] grid) {
-        this.row = grid.length;
-        this.colum = grid[0].length;
         this.grid = grid;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < colum; j++) {
-                if (grid[i][j] == 1) {
+        this.rows = grid.length;
+        this.columns = grid[0].length;
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                if(grid[i][j] == 1) {
                     currentArea = 1;
                     dfs(i, j);
-                    maxArea = Math.max(maxArea, currentArea);
+                    maxArea = Math.max(currentArea, maxArea);
                 }
             }
         }
         return maxArea;
     }
 
-    private void dfs(int i, int j) {
-        grid[i][j] = 0;
+    private void dfs(int row, int column) {
+        grid[row][column] = 0;
 
-        for (int d = 0; d < DIRECTION.length - 1; d++) {
-            int newI = i + DIRECTION[d];
-            int newJ = j + DIRECTION[d + 1];
-            if (newI >= 0 && newI < row
-                    && newJ >= 0 && newJ < colum
-                    && grid[newI][newJ] == 1) {
+        for(int d = 0; d < DIRECTIONS.length - 1; d++) {
+            int newRow = row + DIRECTIONS[d];
+            int newColumn = column + DIRECTIONS[d + 1];
+
+            if(newRow >= 0 && newRow < rows
+                && newColumn >= 0 && newColumn < columns && grid[newRow][newColumn] == 1) {
                 currentArea++;
-                dfs(newI, newJ);
+                dfs(newRow, newColumn);
             }
         }
     }
