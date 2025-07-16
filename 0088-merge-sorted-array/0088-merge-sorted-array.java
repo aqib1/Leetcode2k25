32aec9630e6371f7f0505adc672ed9c926ce6ce3
Aqib Javed <aqibbutt3078@gmail.com>
2025-07-16 04:10:03 +0100

    Time: 0 ms (100%), Space: 42.3 MB (42.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,19 @@
 class Solution {
     // Time complexity O(n + m) and space O(1)
     public void merge(int[] arr, int m, int[] arr1, int n) {
-       int i = m - 1;
+       int k = m + n - 1;
+        int i = m - 1;
         int j = n - 1;
-        int k = m + n - 1;
 
         while(k >= 0) {
             if(j < 0)
                 break;
-            if(i >= 0 && arr[i] > arr1[j]) {
-                arr[k] = arr[i--];
+
+            if(i >= 0 && arr[i] >= arr1[j]) {
+                arr[k--] = arr[i--];
             } else {
-                arr[k] = arr1[j--];
+                arr[k--] = arr1[j--];
             }
-            k--;
         }
     }
 }
