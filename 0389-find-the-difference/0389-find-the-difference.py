f70588a7f5a611a45c650c9165b4808f14938e39
Aqib Javed <aqibbutt3078@gmail.com>
2025-12-19 18:24:16 +0000

    Time: 4 ms (25.78%), Space: 17.1 MB (100%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        charCount = {}
+
+        for ch in s:
+            if ch in charCount:
+                charCount[ch] = charCount[ch] + 1
+            else:
+                charCount[ch] = 1
+
+        for ch in t:
+            if ch not in charCount or charCount[ch] == 0:
+                return ch
+            else:
+                charCount[ch] = charCount[ch] - 1
+
+        return ""
