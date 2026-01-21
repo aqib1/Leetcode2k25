ad2344405da0e1c145b034dfaeb71dab20ddae6c
Aqib Javed <aqibbutt3078@gmail.com>
2026-01-21 22:46:16 +0000

    Time: 1 ms (99.74%), Space: 46.7 MB (26.92%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -12,39 +12,25 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode sum = new ListNode(0);
         ListNode pointer = sum;
-
         int carry = 0;
-        while(l1 != null && l2 != null) {
-            int compound = (l1.val + l2.val) + carry;
 
-            sum.next = new ListNode(compound % 10);
-            carry = compound / 10;
-
-            l1 = l1.next;
-            l2 = l2.next;
-            sum = sum.next;
-        }
-
-        while(l1 != null) {
-            int compound = l1.val + carry;
-
-            sum.next = new ListNode(compound % 10);
-            carry = compound / 10;
-            sum = sum.next;
-            l1 = l1.next;
-        }
-
-        while(l2 != null) {
-            int compound = l2.val + carry;
+        while(l1 != null || l2 != null) {
+            int compound = carry;
+            if(l1 != null) {
+                compound += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                compound += l2.val;
+                l2 = l2.next;
+            }
 
             sum.next = new ListNode(compound % 10);
             carry = compound / 10;
             sum = sum.next;
-            l2 = l2.next;
         }
 
         if(carry != 0) sum.next = new ListNode(carry);
-
         return pointer.next;
     }
 }
