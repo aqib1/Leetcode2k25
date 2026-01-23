5f1c0ae38ca78cf051d58b59746e5885e9bd5d4b
Aqib Javed <aqibbutt3078@gmail.com>
2026-01-23 02:32:27 +0000

    Time: 1 ms (99.51%), Space: 48.8 MB (63.1%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,48 +1,44 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            var tmp = nums1;
-            nums1 = nums2;
-            nums2 = tmp;
+    public double findMedianSortedArrays(int[] num1, int[] num2) {
+        if (num1.length > num2.length) {
+            var tmp = num1;
+            num1 = num2;
+            num2 = tmp;
         }
 
-        int min = 0;
-        int max = nums1.length;
-
+        int minIndex = 0, maxIndex = num1.length;
         int x = 0, y = 0;
-        boolean isOdd = (nums1.length + nums2.length) % 2 != 0;
         double median = 0.0;
+        boolean isOdd = (num1.length + num2.length) % 2 != 0;
 
-        while (min <= max) {
-            x = (min + max) / 2;
-            y = ((nums1.length + nums2.length + 1) / 2) - x;
+        while (minIndex <= maxIndex) {
+            x = (minIndex + maxIndex) / 2;
+            y = ((num1.length + num2.length + 1) / 2) - x;
 
-            if (x > 0 && y < nums2.length && nums1[x - 1] > nums2[y]) {
-                max = x - 1;
-            } else if (y > 0 && x < nums1.length && nums2[y - 1] > nums1[x]) {
-                min = x + 1;
+            if (x > 0 && y < num2.length && num1[x - 1] > num2[y]) {
+                maxIndex = x - 1;
+            } else if (y > 0 && x < num1.length && num2[y - 1] > num1[x]) {
+                minIndex = x + 1;
             } else {
                 if (x == 0) {
-                    median = nums2[y - 1];
+                    median = num2[y - 1];
                 } else if (y == 0) {
-                    median = nums1[x - 1];
+                    median = num1[x - 1];
                 } else {
-                    median = Math.max(nums1[x - 1], nums2[y - 1]);
+                    median = Math.max(num1[x - 1], num2[y - 1]);
                 }
-
                 break;
             }
         }
 
         if (isOdd)
             return median;
-        if (x == nums1.length) {
-            return (median + nums2[y]) / 2.0;
-        }
-        if (y == nums2.length) {
-            return (median + nums1[x]) / 2.0;
-        }
-        return (median + Math.min(nums1[x], nums2[y])) / 2.0;
+        if (x == num1.length)
+            return (median + num2[y]) / 2.0;
+        if (y == num2.length)
+            return (median + num1[x]) / 2.0;
+
+        return (median + Math.min(num1[x], num2[y])) / 2.0;
 
     }
 }
