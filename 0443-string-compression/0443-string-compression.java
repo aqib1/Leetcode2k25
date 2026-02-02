9de09d4a54da226addbdbb490a0ee159741a2cf0
Aqib Javed <aqibbutt3078@gmail.com>
2026-02-02 09:58:09 +0000

    Time: 1 ms (99.65%), Space: 45.4 MB (75.64%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -2,27 +2,26 @@ class Solution {
 
     // Time complexity O(n)
     // Space complexity O(1)
-    public int compress(char[] chars) {
-       int ptr = 0;
-       int count = 0;
+    public int compress(char[] data) {
+       var ptr = 0;
+        var count = 0;
 
-       for(int i = 0; i < chars.length;) {
-           var current = chars[i];
-           chars[ptr++] = current;
+        for(int i = 0; i < data.length;) {
+            var current = data[i];
+            data[ptr++] = current;
+            while(i < data.length && current == data[i]) {
+                count++;
+                i++;
+            }
 
-           while(i < chars.length && chars[i] == current) {
-               count++;
-               i++;
-           }
+            if(count > 1) {
+                for(char ch: String.valueOf(count).toCharArray()) {
+                    data[ptr++] = ch;
+                }
+            }
+            count = 0;
+        }
 
-           if(count > 1) {
-               for(char ch: String.valueOf(count).toCharArray())
-                   chars[ptr++] = ch;
-           }
-
-           count = 0;
-       }
-
-       return ptr;
+        return ptr;
     }
 }
