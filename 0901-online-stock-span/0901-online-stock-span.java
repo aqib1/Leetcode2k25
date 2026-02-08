94cea0391b1c8a47352aedab90419c5e3dba6b66
Aqib Javed <aqibbutt3078@gmail.com>
2026-02-08 21:06:35 +0000

    Time: 31 ms (56.77%), Space: 55.8 MB (16.46%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,18 +1,21 @@
 class StockSpanner {
     private final Queue<int[]> minHeap;
+
     public StockSpanner() {
         this.minHeap = new PriorityQueue<>(
-            Comparator.comparingInt(a -> a[1])
-        );
+                Comparator.comparingInt(a -> a[1]));
     }
 
+
+    // Time complexity O(LogN) and heapify ON(LogN)
+    // Space complexity O(N)
     public int next(int price) {
-       int span = 1;
-       while(!minHeap.isEmpty() && price >=  minHeap.peek()[1]) {
+        int span = 1;
+        while (!minHeap.isEmpty() && price >= minHeap.peek()[1]) {
             span += minHeap.poll()[0];
-       }
-       minHeap.offer(new int[] {span, price});
-       return span;
+        }
+        minHeap.offer(new int[] { span, price });
+        return span;
     }
 }
 
