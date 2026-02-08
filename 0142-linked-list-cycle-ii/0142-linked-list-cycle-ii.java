fffe84a0de792b39dad96158afd08fec826ef341
Aqib Javed <aqibbutt3078@gmail.com>
2026-02-08 04:46:23 +0000

    Time: 0 ms (100%), Space: 47 MB (10.63%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,27 +10,29 @@
  * }
  */
 public class Solution {
+    // Time complexity O(n)
+    // Space complexity O(n)
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null)
+        if(head == null || head.next == null)
             return null;
-        var slow = head;
-        var fast = head;
+
+        var slow = head; var fast = head;
         do {
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast)
-                break;
-        } while (fast != null && fast.next != null);
 
-        if (slow != fast)
-            return null;
+            if(slow == fast) break; // found cycle
+        } while(fast != null && fast.next != null);
+        
+        if(slow != fast) return null; // no cycle found
 
-        slow = head;
-        while (slow != fast) {
+        slow = head; // resetting slow pointer
+
+        while(slow != fast) {
             slow = slow.next;
             fast = fast.next;
         }
 
-        return fast;
+        return slow;     
     }
 }
