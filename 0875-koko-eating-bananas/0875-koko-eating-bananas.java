415d39eba2ba24561d158575f631901493bf83a2
Aqib Javed <aqibbutt3078@gmail.com>
2026-02-13 22:56:38 +0000

    Time: 10 ms (60.85%), Space: 47.8 MB (72.79%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,21 +1,20 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-         int high = Arrays.stream(piles).max().getAsInt();
         var low = 1;
+        var high = Arrays.stream(piles).max().orElse(-1);
 
         while(low < high) {
-            int mid = low + (high - low) / 2;
-            int hours  = 0;
-            for(int pile: piles) {
-                hours += (pile + mid - 1) / mid;
+            var mid = low + (high - low) / 2;
+            var time = 0;
+            for(var pile: piles) {
+                time += (pile + mid - 1) / mid;
             }
-            if(hours <= h) {
+            if(time <= h) {
                 high = mid;
             } else {
                 low = mid + 1;
             }
         }
-
         return low;
     }
 }
