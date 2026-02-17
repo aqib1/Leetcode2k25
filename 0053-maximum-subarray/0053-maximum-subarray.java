6eb1bbd709177e0c35bfef338d39c3806979ce08
Aqib Javed <aqibbutt3078@gmail.com>
2026-02-17 13:05:12 +0000

    Time: 1 ms (99.65%), Space: 77.3 MB (47.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,11 @@
 class Solution {
-
-    // Time complexity O(n)
-    // Space complexity O(1)
     public int maxSubArray(int[] nums) {
-        if(nums == null || nums.length == 0)
-            return -1;
-
-        var max = nums[0]; var sum = nums[0];
-
+        int max = nums[0];
+        int sum = nums[0];
         for(int i = 1; i < nums.length; i++) {
             sum = Math.max(nums[i], sum + nums[i]);
-            if(max < sum)
+
+            if(sum > max)
                 max = sum;
         }
 
