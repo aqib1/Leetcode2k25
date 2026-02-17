d1defecd71ab67902b50202077e2709ece082c9d
Aqib Javed <aqibbutt3078@gmail.com>
2026-02-17 14:54:11 +0000

    Time: 71 ms (75.53%), Space: 127 MB (95.45%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
@@ -1,6 +1,5 @@
 class StockPrice {
-    // Space O(N)
-    private int lastestTimestamp;
+    private int timestamp;
     private final Map<Integer, StockRecord> stockMap;
     private final Queue<StockRecord> minHeap;
     private final Queue<StockRecord> maxHeap;
@@ -11,46 +10,36 @@ class StockPrice {
         this.maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b.price, a.price));
     }
 
-    // OLog(N) if heapify then ON(LogN)
     public void update(int timestamp, int price) {
-        lastestTimestamp = Math.max(lastestTimestamp, timestamp);
+        this.timestamp = Math.max(timestamp, this.timestamp);
         var stockRecord = new StockRecord(timestamp, price);
-
-        stockMap.put(timestamp, stockRecord);
-        minHeap.offer(stockRecord);
-        maxHeap.offer(stockRecord);
+        this.stockMap.put(timestamp, stockRecord);
+        this.minHeap.offer(stockRecord);
+        this.maxHeap.offer(stockRecord);
     }
 
-    // O(1)
     public int current() {
-        return stockMap.get(lastestTimestamp).price;
+        return stockMap.get(timestamp).price();
     }
 
-    // OLog(N)
     public int maximum() {
-        if(stockMap.isEmpty())
-            return -1;
-
-        while(!maxHeap.isEmpty() && maxHeap.peek().price != stockMap.get(maxHeap.peek().timestamp).price) {
+        while (!maxHeap.isEmpty() && (maxHeap.peek().price != stockMap.get(maxHeap.peek().timestamp).price)) {
             maxHeap.poll();
         }
 
         return maxHeap.isEmpty() ? -1 : maxHeap.peek().price;
     }
 
-    // OLog(N)
     public int minimum() {
-        if(stockMap.isEmpty())
-            return -1;
-
-        while(!minHeap.isEmpty() && minHeap.peek().price != stockMap.get(minHeap.peek().timestamp).price) {
+        while (!minHeap.isEmpty() && (minHeap.peek().price != stockMap.get(minHeap.peek().timestamp).price)) {
             minHeap.poll();
         }
 
         return minHeap.isEmpty() ? -1 : minHeap.peek().price;
     }
 
-    record StockRecord(int timestamp, int price) {}
+    record StockRecord(int timestamp, int price) {
+    }
 }
 
 /**
