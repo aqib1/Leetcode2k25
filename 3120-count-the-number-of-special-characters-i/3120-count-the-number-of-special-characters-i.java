3b5d1adad5964b97b56c290466586662b60b8847
Aqib Javed <aqibbutt3078@gmail.com>
2026-05-27 01:27:27 +0100

    Time: 1 ms (100%), Space: 43.4 MB (90.15%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,46 +1,18 @@
 class Solution {
-    static class SpecialChar {
-        private boolean isLower;
-        private boolean isUpper;
-
-        public void foundLower() {
-            isLower = true;
-        }
-
-        public void foundUpper() {
-            isUpper = true;
-        }
-
-        public boolean isSpecial() {
-            return isLower && isUpper;
-        }
-    }
-
     public int numberOfSpecialChars(String word) {
-        var map = new HashMap<Character, SpecialChar>();
         int count = 0;
+        boolean []upper = new boolean[26];
+        boolean []lower = new boolean[26];
 
-        for(char ch: word.toCharArray()) {
-            if(map.containsKey(Character.toLowerCase(ch))) {
-                var specialChar = map.get(Character.toLowerCase(ch));
-                if(Character.isUpperCase(ch)) {
-                    specialChar.foundUpper();
-                } else {
-                    specialChar.foundLower();
-                }
-            } else {
-                var specialChar = new SpecialChar();
-                if(Character.isUpperCase(ch)) {
-                    specialChar.foundUpper();
-                } else {
-                    specialChar.foundLower();
-                }
-                map.put(Character.toLowerCase(ch), specialChar);
-            }
+        for(char c : word.toCharArray()) {
+            if(Character.isUpperCase(c))
+                upper[c - 'A'] = true;
+            else
+                lower[c - 'a'] = true;
         }
 
-        for(var key: map.keySet()) {
-            if(map.get(key).isSpecial()) {
+        for(int i = 0; i < 26; i++) {
+            if(upper[i] && lower[i]) {
                 count++;
             }
         }
