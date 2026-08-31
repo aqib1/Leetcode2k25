4147d4a0f1a47d8c8e1e2ecb4118a6975c630e5b
Aqib Javed <aqibbutt3078@gmail.com>
2026-08-31 22:13:02 +0100

    Time: 2 ms (99.34%), Space: 47.5 MB (11.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        var complimentMap = new HashMap<Integer, Integer>();
-        for(int n = 0; n < nums.length; n++) {
-            int compliment = target - nums[n];
-            if(complimentMap.containsKey(compliment)) {
-                return new int[] {complimentMap.get(compliment), n};
-            } else {
-                complimentMap.put(nums[n], n);
+        var complimentByIndex = new HashMap<Integer, Integer>();
+            for(int i = 0; i < nums.length; i++) {
+                int compliment = target - nums[i];
+                if(complimentByIndex.containsKey(compliment)) {
+                    return new int[] {complimentByIndex.get(compliment), i};
+                }
+                complimentByIndex.put(nums[i], i);
             }
-        }
-
-        return null;
+            return null;
     }
 }
