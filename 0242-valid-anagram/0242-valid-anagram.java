0e32645ab7faf2ff79978b009fc04d503d9a89f3
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-08 00:55:46 +0500

    Time: 5 ms (63.04%), Space: 44.4 MB (74.37%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        var ch = new char[26];
+        for (int i = 0; i < s.length(); i++) {
+            ch[s.charAt(i) - 'a']++;
+            ch[t.charAt(i) - 'a']--;
+        }
+        for (int c : ch) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
