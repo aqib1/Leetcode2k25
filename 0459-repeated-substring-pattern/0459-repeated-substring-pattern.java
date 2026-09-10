2641ca714ed1cc0f2463ee49b96e324a360de64a
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-11 04:18:37 +0500

    Time: 4 ms (99.74%), Space: 46.5 MB (87.05%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,30 +1,32 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        int n = s.length();
-        int[] lpsArray = lps(s);
-        int lastLPS = lpsArray[n - 1];
-        int patternLength = n - lastLPS;
-        return lastLPS > 0 && n % patternLength == 0;
+        var lps = lps(s);
+        var n = s.length();
+        var lastN = lps[lps.length - 1];
+        var k = n - lastN;
+        return lastN != 0 && n % k == 0;
     }
 
-    private int[] lps(String str) {
-        var chars = str.toCharArray();
-        var lps = new int[str.length()];
+    public int[] lps(String value) {
+        if(value == null || value.isEmpty())
+            return null;
+        var chars = value.toCharArray();
+        var lps = new int[value.length()];
         var len = 0; var i = 1;
-
         while(i < chars.length) {
             if(chars[len] == chars[i]) {
                 lps[i] = ++len;
                 i++;
             } else {
-                if(len != 0)
-                    len = lps[len-1];
-                else {
+                if(len != 0) {
+                    len = lps[len - 1];
+                } else {
                     lps[i] = 0;
                     i++;
                 }
             }
         }
+
         return lps;
     }
 }
