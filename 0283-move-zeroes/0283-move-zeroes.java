b0165a951d3a7ec6fe17bf1fb5d73b380197ee96
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-11 04:56:53 +0500

    Time: 46 ms (5.28%), Space: 47.6 MB (87.81%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,11 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        var newNum = new int[nums.length];
-        int i = 0;
-        for(var n: nums) {
-            if(n != 0)
-                newNum[i++] = n;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                for(int j = i + 1; j < nums.length; j++) {
+                    if(nums[j] != 0) {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        break;
+                    }
+                }
+            }
         }
-        System.arraycopy(newNum, 0, nums, 0, nums.length);
     }
 }
