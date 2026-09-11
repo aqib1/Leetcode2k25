9db58f1938395f6c1423c01671d00431edc6a005
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-11 05:52:09 +0500

    Time: 0 ms (100%), Space: 43.7 MB (8.52%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        var count = new ArrayList<Integer>();
-        var add = 1;
-        for(int i = digits.length - 1; i >= 0; i--) {
-            var sum = digits[i] + add;
-            add = sum / 10;
-            count.addFirst(sum % 10);
+        for(var i=digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-
-        if(add > 0) {
-            count.addFirst(add);
-        }
-
-        return count.stream().mapToInt(Integer::intValue).toArray();
+        var sum = new int[digits.length + 1];
+        sum[0] = 1;
+        return sum;
     }
 }
