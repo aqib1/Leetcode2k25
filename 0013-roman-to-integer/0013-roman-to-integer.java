28035672a2947a6fb21c16870ddcdf1a3f274cb4
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-11 08:38:38 +0500

    Time: 7 ms (4.5%), Space: 46.9 MB (20%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,40 +1,30 @@
 class Solution {
-    private static final Map<String, Integer> ROMAN_MAP = new HashMap<>();
-
-    static {
-        ROMAN_MAP.put("I", 1);
-        ROMAN_MAP.put("V", 5);
-        ROMAN_MAP.put("X", 10);
-        ROMAN_MAP.put("IV", 4);
-        ROMAN_MAP.put("IX", 9);
-        ROMAN_MAP.put("L", 50);
-        ROMAN_MAP.put("C", 100);
-        ROMAN_MAP.put("XL", 40);
-        ROMAN_MAP.put("XC", 90);
-        ROMAN_MAP.put("D", 500);
-        ROMAN_MAP.put("M", 1000);
-        ROMAN_MAP.put("CD", 400);
-        ROMAN_MAP.put("CM", 900);
-    }
-
-    // Time complexity O(n) and space O(1)
     public int romanToInt(String s) {
-        int number = 0;
-        int i = 0;
-        while(i < s.length()) {
-            String key;
-            if(i + 1 < s.length()) {
-                 key = s.substring(i, i + 2);
-                if(ROMAN_MAP.containsKey(key)) {
-                    i += 2;
-                    number += ROMAN_MAP.get(key);
-                    continue;
-                }
+        var romainPair = Map.of(
+                'I', Set.of('V', 'X'),
+                'X', Set.of('L', 'C'),
+                'C', Set.of('D', 'M'));
+        var romanMap = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000);
+        var val = 0;
+        var i = 0;
+        while (i < s.length()) {
+            var ch = s.charAt(i);
+            if (i + 1 < s.length() && romainPair.containsKey(ch)
+                    && romainPair.get(ch).contains(s.charAt(i + 1))) {
+                val += romanMap.get(s.charAt(i + 1)) - romanMap.get(ch);
+                i += 2;
+            } else {
+                val += romanMap.get(ch);
+                i++;
             }
-            key = s.substring(i, i + 1);
-            number += ROMAN_MAP.get(key);
-            i++;
         }
-        return number;
+        return val;
     }
 }
