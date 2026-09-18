aab12d64ebe6b2bd157c69f5bdbf8ba5785fd2b6
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-18 07:53:42 +0500

    Time: 79 ms (0.95%), Space: 45.5 MB (86.19%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,29 +1,15 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 class Solution {
     public double average(int[] salary) {
-       var minHeap = new PriorityQueue<int[]>(
-                Comparator.comparingInt(a -> a[1])
-        );
-
-        var maxHeap = new PriorityQueue<int[]>(
-                (a, b) -> Integer.compare(b[1], a[1])
-        );
-
-        for(int i = 0; i < salary.length; i++) {
-            minHeap.offer(new int[]{i, salary[i]});
-            maxHeap.offer(new int[] {i, salary[i]});
-        }
-
-        var min = minHeap.poll();
-        var max = maxHeap.poll();
+       var min = salary[0];
+        var max = salary[0];
         var sum = 0;
-        for(int i = 0; i < salary.length; i++) {
-            if(i != min[0] && i != max[0]) {
-                sum += salary[i];
-            }
+
+        for(var s: salary) {
+            sum += s;
+            min = Math.min(min, s);
+            max = Math.max(max, s);
         }
-        return (double) sum / (salary.length - 2); 
+
+        return (double)(sum - max - min) / (salary.length - 2);
     }
 }
