06134f881158eb71a2ecdf856686147c4bdcf995
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-19 19:48:43 +0500

    Time: 0 ms (100%), Space: 45.2 MB (44.26%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        var dx = coordinates[1][0] - coordinates[0][0];
+        var dy = coordinates[1][1] - coordinates[0][1];
+
+        for (int i = 1; i < coordinates.length; i++) {
+            var xi = coordinates[i][0] - coordinates[i - 1][0];
+            var yi = coordinates[i][1] - coordinates[i - 1][1];
+
+            if (dx * yi != dy * xi)
+                return false;
+        }
+
+        return true;
+    }
+}
