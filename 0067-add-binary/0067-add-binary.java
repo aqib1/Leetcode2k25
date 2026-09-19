db20639b1ef931aed514bf5a63c585c607aba4a5
Aqib Javed <aqibbutt3078@gmail.com>
2026-09-19 16:57:20 +0100

    Time: 1 ms (99.98%), Space: 43.8 MB (22.88%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        var builder = new StringBuilder();
+
+        var carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while(i >= 0 || j >= 0 || carry > 0) {
+            if (i >= 0) {
+                carry += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                carry += b.charAt(j--) - '0';
+            }
+            builder.append(carry % 2);
+            carry /= 2;
+        }
+
+        return builder.reverse().toString();
+    }
+}
